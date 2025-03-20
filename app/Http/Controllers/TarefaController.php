4e83c202186efcccbd2b3fb4ae90bb2ed392adad
Natygabriela <nataliaggb0007@gamail.com>
2025-03-20 09:23:00 -0300

    adionar tarefa controller

--- app/Http/Controllers/TarefaController.php
+++ app/Http/Controllers/TarefaController.php
@@ -6,5 +6,38 @@ use Illuminate\Http\Request;
 
 class TarefaController extends Controller
 {
-    //
+    public function store(Request $request){
+
+} 
+
+public function index(){
+
+}
+
+public function show(){
+
+}
+
+public function update(Request $request, string $id){
+
+}
+
+public function destroy(string $id){
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
